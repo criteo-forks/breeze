697bc16ee70067cc9c88632890b48c2e358e2ae5
Sergei Lebedev <s.lebedev@criteo.com>
2018-02-14 23:12:31 +0100

    Small cleanup in OpenAddressHashArray

--- math/src/main/scala/breeze/collection/mutable/OpenAddressHashArray.scala
+++ math/src/main/scala/breeze/collection/mutable/OpenAddressHashArray.scala
@@ -18,14 +18,17 @@ package breeze.collection.mutable
 
 import java.util
 
-import breeze.storage.{ConfigurableDefault, Storage, Zero}
-
 import scala.reflect.ClassTag
 import scala.util.hashing.MurmurHash3
 
+import breeze.storage.{ConfigurableDefault, Storage, Zero}
+import breeze.util.ArrayUtil
+import spire.syntax.cfor._
+
 /**
- * This is a Sparse Array implementation backed by a linear-probing
- * open address hash table.
+ * Open addressing hash table with linear probing.
+ *
+ * The maintained load factor is 0.75.
  *
  * @author dlwh
  */
@@ -85,14 +88,14 @@ final class OpenAddressHashArray[@specialized(Int, Float, Long, Double) V] priva
 
   def allVisitableIndicesActive = false
 
-  final def apply(i: Int) = {
-    if (i < 0 || i >= size) throw new IndexOutOfBoundsException()
+  def apply(i: Int) = {
+    checkIndex(i)
     if (index.length == 0) default.value
     else data(locate(i))
   }
 
-  final def update(i: Int, v: V) {
-    if (i < 0 || i >= size) throw new IndexOutOfBoundsException(i + " is out of bounds for size " + size)
+  def update(i: Int, v: V) {
+    checkIndex(i)
     val pos = locate(i)
     _data(pos) = v
     if (_index(pos) != i) {
@@ -111,46 +114,35 @@ final class OpenAddressHashArray[@specialized(Int, Float, Long, Double) V] priva
   def activeIterator = index.iterator.zip(data.iterator).filter(_._1 >= 0)
 
   private def locate(i: Int) = {
-    if (i >= size) throw new IndexOutOfBoundsException(i + " greater than size of " + size)
-    if (i < 0) throw new IndexOutOfBoundsException(i + " less than 0")
+    // Invariant: index is valid as defined by [[checkIndex]].
     val index = this.index
     val len = index.length
     var hash = hashCodeFor(i) & (len - 1)
     while (index(hash) != i && index(hash) >= 0) {
-      hash += 1
-      if (hash >= len) {
-        hash = 0
-      }
+      hash = (hash + 1) & (len - 1)
     }
     hash
   }
 
   private def hashCodeFor(i: Int): Int = {
-    // based on what's in HashTable.scala and scala.util.hashing (inlined because of 2.9.2 support)
-    // doing this so that i and i +1 aren't near each other.
-    var code = i.##
-    code *= 0x9e3775cd
-    code = java.lang.Integer.reverseBytes(code)
-    code *= 0x9e3775cd
-    val rotated = (code >>> 11) | (code << 21)
-    rotated
+    // See [[scala.collection.mutable.HashTable.improve]].
+    val code = scala.util.hashing.byteswap32(i)
+    val rotation = 11
+    (code >>> rotation) | (code << (32 - rotation))
   }
 
   final protected def rehash() {
     val oldIndex = index
     val oldValues = data
-    val newSize = OpenAddressHashArray.calculateSize(oldIndex.size + 1)
-    _index = new Array[Int](newSize)
-    util.Arrays.fill(_index, -1)
+    val newSize = OpenAddressHashArray.calculateSize(oldIndex.length + 1)
+    _index = OpenAddressHashArray.emptyIndexArray(newSize)
     _data = new Array[V](newSize)
     default.fillArray(_data, default.value)
     load = 0
-    var i = 0
-    while (i < oldIndex.length) {
+    cforRange(0 until oldIndex.length) { i =>
       if (oldIndex(i) >= 0) {
         update(oldIndex(i), oldValues(i))
       }
-      i += 1
     }
   }
 
@@ -165,7 +157,7 @@ final class OpenAddressHashArray[@specialized(Int, Float, Long, Double) V] priva
   def copy: OpenAddressHashArray[V] = {
     new OpenAddressHashArray[V](
       util.Arrays.copyOf(_index, _index.length),
-      breeze.util.ArrayUtil.copyOf(_data, _data.length),
+      ArrayUtil.copyOf(_data, _data.length),
       load,
       size,
       default)
@@ -203,6 +195,12 @@ final class OpenAddressHashArray[@specialized(Int, Float, Long, Double) V] priva
       false
   }
 
+  @inline
+  private def checkIndex(i: Int): Unit = {
+    if (i < 0 || i >= size) {
+      throw new IndexOutOfBoundsException(s"index $i is out of bounds for size $size")
+    }
+  }
 }
 
 object OpenAddressHashArray {
